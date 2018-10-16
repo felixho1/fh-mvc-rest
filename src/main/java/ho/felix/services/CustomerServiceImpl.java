2ae9fd820d4fca302b80fd05a3e4c5cb14742a09
Felix Ho <felix.ho@nab.com.au>
2018-10-16 17:58:40 +1100

    Create new Customer with POST

--- src/main/java/ho/felix/services/CustomerServiceImpl.java
+++ src/main/java/ho/felix/services/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package ho.felix.services;
 
 import ho.felix.api.v1.mapper.CustomerMapper;
 import ho.felix.api.v1.model.CustomerDTO;
+import ho.felix.domain.Customer;
 import ho.felix.repositories.CustomerRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,15 +24,32 @@ public class CustomerServiceImpl implements CustomerService {
         return customerRepository
                 .findAll()
                 .stream()
-                .map(customerMapper::customerToCustomerDTO)
+                .map(customer -> {
+                    CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+                    customerDTO.setCustomerUrl("/api/v1/customers/" + customer.getId());
+                    return customerDTO;
+                })
                 .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO getCustomerById(Long id) {
         return customerRepository.findById(id)
-                .map(customerMapper::customerToCustomerDTO)
+                .map(customer -> {
+                    CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+                    customerDTO.setCustomerUrl("/api/v1/customers/" + customer.getId());
+                    return customerDTO;
+                })
                 .orElseThrow(RuntimeException::new); //todo implement better exception handling
 
     }
+
+    @Override
+    public CustomerDTO createNewCustomer(CustomerDTO customerDTO) {
+        Customer savedCustomer = customerRepository.save(customerMapper.customerDTOToCustomer(customerDTO));
+        CustomerDTO returnDto = customerMapper.customerToCustomerDTO(savedCustomer);
+        returnDto.setCustomerUrl("/api/v1/customers/" + savedCustomer.getId());
+        return returnDto;
+    }
+
 }
