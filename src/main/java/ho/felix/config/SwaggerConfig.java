9d3da773765c6d44416f5c05fed4dc24a65f991f
Felix Ho <felix.ho@nab.com.au>
2018-10-17 14:43:21 +1100

    Swagger Metadata

--- src/main/java/ho/felix/config/SwaggerConfig.java
+++ src/main/java/ho/felix/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,9 +23,25 @@ public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
 
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Felix Ho", "http://felixho.melbourne/about/",
+                "felixho@gmail.com");
+
+        return new ApiInfo(
+                "Spring REST MVC",
+                "REST By Spring MVC Spring Boot 2 Spring 5",
+                "1.0",
+                "Terms of Service: blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
 //    @Override
 //    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("swagger-ui.html")
